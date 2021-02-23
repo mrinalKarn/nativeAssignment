cd2c3629db986efa9b5057578c4c78a92f162ae6
Administrator <oyo@OYOs-MacBook-Pro.local>
2021-02-23 23:44:39 +0530

    project initialized

---
+++ App/modules/userPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+const UserPage = ( ) => {
+    return(
+        <View>
+            <Text>Hello UserPage</Text>
+        </View>
+    )
+}
+
+export default UserPage;
